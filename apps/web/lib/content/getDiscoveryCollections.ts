5d13af18580d3aa2946744ae103fcb28db5a2efd
ezelburkay <ezelburkay@icloud.com>
2026-03-28 23:56:05 +0300

    Add discovery content access utility

---
+++ apps/web/lib/content/getDiscoveryCollections.ts
@@ -0,0 +1,9 @@
+import { personalizedCollections, seasonalCollections } from "@/../packages/content/discovery";
+
+export function getSeasonalCollections() {
+  return seasonalCollections;
+}
+
+export function getPersonalizedCollections() {
+  return personalizedCollections;
+}
