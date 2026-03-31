324eed417fc59540809a624f0aca69f37b325659
ezelburkay <ezelburkay@icloud.com>
2026-03-31 16:29:39 +0300

    Update OnboardingExperience.tsx

--- apps/web/components/onboarding/OnboardingExperience.tsx
+++ apps/web/components/onboarding/OnboardingExperience.tsx
@@ -126,7 +126,9 @@ export function DiscoveryExperience({ onComplete, onSkip }: DiscoveryExperienceP
                 >
                   <span className="onboarding-chip__content">
                     <span className="onboarding-chip__icon-slot">
-                      <span className={`onboarding-chip__icon ${iconClassName}`}>{option.icon}</span>
+                      <span className="onboarding-chip__icon">
+                        <span className={`onboarding-chip__icon-glyph ${iconClassName}`}>{option.icon}</span>
+                      </span>
                     </span>
                     <span className="onboarding-chip__label">{option.label}</span>
                   </span>
