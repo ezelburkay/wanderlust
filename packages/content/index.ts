e8ca7e705ae06164587926289ce62fcb16ecf6ee
ezelburkay <ezelburkay@icloud.com>
2026-04-10 13:28:01 +0300

    Update index.ts

--- packages/content/index.ts
+++ packages/content/index.ts
@@ -1,34 +1,5 @@
-export { paris } from "./paris";
-export { rome } from "./rome";
-export { kyoto } from "./kyoto";
-export { lisbon } from "./lisbon";
-export { vienna } from "./vienna";
-export { copenhagen } from "./copenhagen";
-export { sanSebastian } from "./san-sebastian";
-export { oaxaca } from "./oaxaca";
-export { seville } from "./seville";
-export { reykjavik } from "./reykjavik";
+export * from "./schemas/city";
+export * from "./schemas/discovery";
 
-import { paris } from "./paris";
-import { rome } from "./rome";
-import { kyoto } from "./kyoto";
-import { lisbon } from "./lisbon";
-import { vienna } from "./vienna";
-import { copenhagen } from "./copenhagen";
-import { sanSebastian } from "./san-sebastian";
-import { oaxaca } from "./oaxaca";
-import { seville } from "./seville";
-import { reykjavik } from "./reykjavik";
-
-export const cities = [
-  paris,
-  rome,
-  kyoto,
-  lisbon,
-  vienna,
-  copenhagen,
-  sanSebastian,
-  oaxaca,
-  seville,
-  reykjavik
-];
+export * from "./cities";
+export * from "./discovery";
